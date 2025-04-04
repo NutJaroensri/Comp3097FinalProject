891b77119eaae580adbe75beae79694c84ce9163
Nut Jaroensri <nutjaroensri@Nuts-MacBook-Air.local>
2025-04-03 22:17:20 -0400

    Initial commit of new project

---
+++ Comp3097FinalProject/Comp3097FinalProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  Comp3097FinalProjectApp.swift
+//  Comp3097FinalProject
+//
+//  Created by Nut Jaroensri on 2025-04-03.
+//
+
+import SwiftUI
+
+@main
+struct Comp3097FinalProjectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
